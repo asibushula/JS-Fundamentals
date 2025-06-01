889cf02870bd95c65b40fa8d8b23d89f38b018a2
Asiphe Bushula <asiphebushula99@gmail.com>
2025-06-01 22:34:36 +0200

    Added file 5-interger that prints if an argument is an interger

---
+++ 5-interger.js
@@ -0,0 +1,10 @@
+#!/usr/bin.Node
+
+const { argv } = require('process');
+
+const converted = Number(argv[2]);
+if (isNaN(converted)) {
+    console.log('Not a number'); 
+}   else {
+    console.log('My number: ${converted}');
+}
