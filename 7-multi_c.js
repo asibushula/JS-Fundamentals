c4e21eb7c593dabbe6201ebdefefcc00e806aa76
Asiphe Bushula <asiphebushula99@gmail.com>
2025-06-01 22:53:44 +0200

    Added file 7-multi_c that prints a statement x times

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = requir('process');
+
+if (isNaN(process.argv[2])) {
+    console.log('Missing number of occurunces');
+}   else {
+    for (let i = 0; i < process.argv[2]; i++) {
+        console.log('C is fun');
+    }
+}
