baf2152db0dcd3c1c0bc6fa551a42aeaf46e3e83
Asiphe Bushula <asiphebushula99@gmail.com>
2025-06-01 21:57:23 +0200

    Added 2-arguments file with script that prints message based on arguments passed

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,12 @@
-const { argv } = require('node:process');
+#!/usr/bin/node
 
-if (parseArgs.length === 0) {
-    console.log("No arguments");
-}   else if (parseArgs.length === 1) {
-    console.log("Arguments found");
+const { argv } = require('process');
+const argc = argv.length;
+
+if (argc === 0) {
+    console.log('No arguments');
+}   else if (argc === 1) {
+    console.log('Argument found');
 }   else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
